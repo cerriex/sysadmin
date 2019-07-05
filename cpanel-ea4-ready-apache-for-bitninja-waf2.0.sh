bb870b76663125de18d06cb2095e18ab4a921429
cerriex <cerriex@gmail.com>
2019-07-05 09:29:56 +0100

    Create cpanel-ea4-ready-apache-for-bitninja-waf2.0.sh

---
+++ cpanel-ea4-ready-apache-for-bitninja-waf2.0.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+# C-Russell 05/07/2019
+# Setup cPanel provided Apache (EA4) for Bitninja WAF 2.0
+# Check and install mod_remoteip if not already installed
+if ! rpm -qa | grep -q remoteip; then
+  yum -y install ea-apache24-mod_remoteip.x86_64
+fi
+
+# Edit remoteip.conf file
+MODFILE=$(find /etc/apache2/conf.modules.d/ -name "*mod_remoteip.conf")
+cat >> $MODFILE <<EOL
+<IfModule remoteip_module>
+ RemoteIPHeader X-Forwarded-For
+ RemoteIPTrustedProxy 127.0.0.1 $(hostname -i)
+</IfModule>
+EOL
+
+# Set logging for forwarded IP
+cp /etc/apache2/conf.d/includes/pre_main_global.conf{,.pre-waf2.0}
+cat >> /etc/apache2/conf.d/includes/pre_main_global.conf <<EOL
+<IfModule remoteip_module>
+  RemoteIPHeader X-Forwarded-For
+</IfModule>
+<IfModule log_config_module>
+  LogFormat "%{Referer}i -> %U" referer
+  LogFormat "%{User-agent}i" agent
+  LogFormat "%a %l %u %t \"%r\" %>s %b" common
+  LogFormat "%a %l %u %t \"%r\" %>s %b \"%{Referer}i\" \"%{User-Agent}i\"" combined
+  CustomLog logs/access_log combined
+</IfModule>
+EOL
+
+# Restart Apache
+service httpd restart
+
+# Done
+echo -e "Apache has been prepared for WAF 2.0. You may now enable it inside the Bitninja control panel."
